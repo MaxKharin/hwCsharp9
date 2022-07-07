31a3c71215cd569ab5787e380153e3c9b950e1bf
MaximKharin <maximkharin@gmail.com>
2022-07-07 16:55:32 +0300

    finished task1

---
+++ task1/Program.cs
@@ -0,0 +1,66 @@
+﻿// Задача 1: Задайте двумерный массив.
+// Напишите программу, которая упорядочит по убыванию
+// элементы каждой строки двумерного массива.
+
+int row = Prompt("Rows number: ");
+int column = Prompt("Columns number: ");
+int min = 0;
+int max = 10;
+int[,] array = InitArray(row, column, min, max);
+PrintArray(array);
+
+Console.WriteLine("Sorted array: ");
+SortArrayRows(array);
+PrintArray(array);
+
+void SortArrayRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+int[,] InitArray(int row, int column, int min, int max)
+{
+    var array = new int[row, column];
+    var rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
